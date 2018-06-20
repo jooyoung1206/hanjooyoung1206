888389b7c443dbd787699332f406b604af29454c
hanjooyoung <hanjooyoung1206@gmail.com>
2018-06-20 14:40:25 +0900

    jooyoung4

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,52 +1,22 @@
 <template>
   <div>
     <nuxt/>
+    <section class="section">
+        <div class="container">
+          <h1 class="title">POT LOVER</h1>
+          <h2 class="subtitle">
+            POT LOVER는 식물을 기르면서 현대인들의 고독과 외로움을 치유 할 수 있습니다.
+          </h2>
+        </div>
+        <section class="section"></section>
+    <div class="container"></div>
+    <h1 class="title">식물이란 무엇인가?
+      일반적으로, 운동 기관이 없어 자유롭게 움직일 수 없고 광합성을 할 수 있으며 세포에 세포벽이 있는, 생물의 하나
+    </h1>
+    <figure class="image is-128x128">
+        <img src="123.jpg" alt="">
+      </figure>
+      </section>
   </div>
 </template>
 
-<style>
-html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*, *:before, *:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -12,21 +12,9 @@
       </div>
     </div>
     <body>
-      <section class="section">
-        <div class="container">
-          <h1 class="title">POT LOVER</h1>
-          <h2 class="subtitle">
-            POT LOVER는 식물을 기르면서 현대인들의 고독과 외로움을 치유 할 수 있습니다.
-          </h2>
-        </div>
-      </section>
     </body>
-    <section class="section"></section>
-    <div class="container"></div>
-    <h1 class="title">식물이란 무엇인가?
-      일반적으로, 운동 기관이 없어 자유롭게 움직일 수 없고 광합성을 할 수 있으며 세포에 세포벽이 있는, 생물의 하나
-    </h1>
-
+    <article class="message"></article>
+        
   </section>
   
     </template>
